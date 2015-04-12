ffc6e974e57d435ae627b1598912cf8a9abb2052
Ben817 <blennartson@yahoo.se>
2015-04-12 14:52:26 +0200

    plot1

---
+++ plot1.R
@@ -0,0 +1,23 @@
+#Set working directory
+setwd("~/Documents/Diverse/Coursera Exploratory Data Analysis/Course Project 1/ExData_Plotting1/")
+
+#Read data
+df <-read.csv2(file ="../household_power_consumption.txt",sep = ";",dec = ".",header=T,colClasses=c(rep("character",9)))
+
+#Format Date column and fiter out correct dates
+df$Date <- strptime(df$Date,format = "%d/%m/%Y")
+startdate <- strptime("1/2/2007",format = "%d/%m/%Y")
+enddate <- strptime("2/2/2007",format = "%d/%m/%Y")
+df<-df[df$Date==startdate|df$Date==enddate,]
+
+#Remove large dataset and collect garbage
+gc()
+
+#Change from character to numeric
+df$Global_active_power <- as.numeric(df$Global_active_power)
+
+#
+with(df,hist(Global_active_power,col = "red",xlab = c("Global Active Power (kilowatts)")))
+title(main = "Global Active Power")
+dev.copy(png, file = "plot1.png")
+dev.off()
